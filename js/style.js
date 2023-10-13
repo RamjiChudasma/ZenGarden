f41983de05d872b18679a6f3cb86a1f2f03c45cb
RamjiChudasma <ramjichudasma456@gmail.com>
2023-10-13 17:42:37 +0530

    first

---
+++ js/style.js
@@ -0,0 +1,64 @@
+$('#slider1').owlCarousel({
+    loop:true,
+    // autoplay:true,
+    autoplayTimeout:2000,
+    items:1,
+    margin:10,
+    nav:true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    //     1000:{
+    //         items:5
+    //     }
+    navTex:['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+    dots:false,
+    }
+    
+
+)
+// slider 2 start
+$('#slider2').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayHoverPause:true,
+    autoplayTimeout:1500,
+    items:4,
+    nav:true,
+    dots:false,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    //     1000:{
+    //         items:4
+    //     }
+    // }
+})
+$('#slider4').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:1500,
+})
+$('#slider5').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:5,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    
+})
